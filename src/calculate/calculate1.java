13301f3281f8715ef07169aa172f3ec5d47c373e
AshokElancheliyan <ashokelancheliyan@gmail.com>
2024-01-30 17:37:33 +0530

    dfdfd

--- src/calculate/calculate1.java
+++ src/calculate/calculate1.java
@@ -5,10 +5,7 @@ public class calculate1 {
 
 	public void add(int c1,int c2) {
    System.out.println(c1+c2);
-   System.out.println("fine");
-   System.out.println("hru");
-   System.out.println("wru");
-   System.out.println("ghdghaggas");
-   System.out.println("bbbbb");
+   System.out.println("fffff");
+   System.out.println("hhhhccc");
 }
 }
