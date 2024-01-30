caee8bce616db28e5a11cf3834d665dabbc7d099
AshokElancheliyan <ashokelancheliyan@gmail.com>
2024-01-30 16:53:08 +0530

    second project

--- src/calculate/calculate1.java
+++ src/calculate/calculate1.java
@@ -5,6 +5,8 @@ public class calculate1 {
 
 	public void add(int c1,int c2) {
    System.out.println(c1+c2);
-   System.out.println("hello");
+   System.out.println("fine");
+   System.out.println("hru");
+   System.out.println("wru");
 }
 }
